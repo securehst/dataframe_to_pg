13086a3d8de34bfff101559144f3108a6e6462db
semantic-release <semantic-release>
2026-08-11 18:24:30 +0000

    1.0.1
    Automatically generated by python-semantic-release

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ from sphinx.ext import apidoc
 project = "Dataframe to Postgres"
 copyright = "2025, Mark Moreno"
 author = "Mark Moreno"
-release = "1.0.1-rc.1"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
